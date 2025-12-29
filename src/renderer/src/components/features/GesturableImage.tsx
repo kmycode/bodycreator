5ad257225bab083625e179893d5b7a6ce460fe99
tt@kmycode.net <tt@kmycode.net>
2025-12-29 09:53:11 +0900

    Add: 回転、水平反転、垂直反転機能（UI）

--- src/renderer/src/components/features/GesturableImage.tsx
+++ src/renderer/src/components/features/GesturableImage.tsx
@@ -1,9 +1,17 @@
 import { useSpring, animated } from '@react-spring/web';
-import { updateMatrix } from '@renderer/models/entities/image_preview_matrix';
+import {
+  getExtraMatrix,
+  getSpringMatrix,
+  ImagePreviewMatrixes,
+  ImagePreviewMatrixForExtra,
+  updateMatrixForExtra,
+  updateMatrixForSpring,
+} from '@renderer/models/entities/image_preview_matrix';
 import { useAppDispatch, useAppSelector } from '@renderer/models/store';
 import { useGesture } from '@use-gesture/react';
 import classNames from 'classnames';
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
 
 const GesturableImage: React.FC<{
   imageId: number;
@@ -17,18 +25,12 @@ const GesturableImage: React.FC<{
     scale: firstScale,
     x: firstX,
     y: firstY,
-    reverseHorizontal,
-    reverseVertical,
-    rotate,
-  } = useAppSelector((state) => state.imagePreviewMatrixes.matrixes.find((m) => m.imageId === imageId)) ?? {
-    imageId,
-    x: undefined,
-    y: undefined,
-    scale: undefined,
-    reverseHorizontal: false,
-    reverseVertical: false,
-    rotate: 0,
-  };
+  } = useSelector((state) =>
+    getSpringMatrix(state as { imagePreviewMatrixes: ImagePreviewMatrixes }, imageId),
+  );
+  const { rotate, reverseHorizontal, reverseVertical } = useSelector((state) =>
+    getExtraMatrix(state as { imagePreviewMatrixes: ImagePreviewMatrixes }, imageId),
+  );
 
   const dispatch = useAppDispatch();
 
@@ -41,10 +43,29 @@ const GesturableImage: React.FC<{
     [firstScale, firstX, firstY],
   );
 
+  const updateExtraMatrix = useCallback(
+    (merge: Partial<ImagePreviewMatrixForExtra>) => {
+      dispatch(
+        updateMatrixForExtra({
+          imageId,
+          rotate,
+          reverseHorizontal,
+          reverseVertical,
+          ...merge,
+        }),
+      );
+    },
+    [dispatch, imageId, rotate, reverseHorizontal, reverseVertical],
+  );
+
   const bind = useGesture(
     {
-      onDrag: ({ offset: [x, y] }) => {
-        void springRef.start({ x, y });
+      onDrag: ({ offset: [x, y], delta: [dx], shiftKey }) => {
+        if (shiftKey) {
+          updateExtraMatrix({ rotate: rotate + dx * 0.2 });
+        } else {
+          void springRef.start({ x, y });
+        }
       },
 
       onPinch: ({ offset: [s] }) => {
@@ -83,18 +104,16 @@ const GesturableImage: React.FC<{
 
   useEffect(() => {
     return () => {
-      const matrix = {
-        x: style.x.get(),
-        y: style.y.get(),
-        scale: style.scale.get(),
-        reverseHorizontal,
-        reverseVertical,
-        rotate,
-        imageId,
-      };
-      dispatch(updateMatrix(matrix));
+      dispatch(
+        updateMatrixForSpring({
+          imageId,
+          x: style.x.get(),
+          y: style.y.get(),
+          scale: style.scale.get(),
+        }),
+      );
     };
-  }, [imageId, style, dispatch, reverseHorizontal, reverseVertical, rotate]);
+  }, [dispatch, style, imageId]);
 
   const canvasRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
@@ -126,6 +145,14 @@ const GesturableImage: React.FC<{
       });
     }
   }, [fileName, firstX, firstY, firstScale, canvasRef, springRef, imageWidth, imageHeight]);
+
+  const handleReverseHorizontal = useCallback(() => {
+    updateExtraMatrix({ reverseHorizontal: !reverseHorizontal });
+  }, [updateExtraMatrix, reverseHorizontal]);
+
+  const handleReverseVertical = useCallback(() => {
+    updateExtraMatrix({ reverseVertical: !reverseVertical });
+  }, [updateExtraMatrix, reverseVertical]);
 
   return (
     <div className="gesturable-image">
@@ -159,7 +186,11 @@ const GesturableImage: React.FC<{
         </div>
       </div>
 
-      <div className="commands-bar"></div>
+      <div className="commands-bar">
+        <code>Shift + drag</code>回転
+        <button onClick={handleReverseHorizontal}>水平反転</button>
+        <button onClick={handleReverseVertical}>垂直反転</button>
+      </div>
     </div>
   );
 };
--- src/renderer/src/models/entities/image_preview_matrix.ts
+++ src/renderer/src/models/entities/image_preview_matrix.ts
@@ -1,21 +1,40 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-export interface ImagePreviewMatrix {
+export interface ImagePreviewMatrixForSpring {
   imageId: number;
-  scale: number;
-  x: number;
-  y: number;
-  reverseHorizontal: boolean;
-  reverseVertical: boolean;
-  rotate: number;
+  scale: number | undefined;
+  x: number | undefined;
+  y: number | undefined;
 }
 
+export interface ImagePreviewMatrixForExtra {
+  imageId: number;
+  rotate: number;
+  reverseHorizontal: boolean;
+  reverseVertical: boolean;
+}
+
+export interface ImagePreviewMatrix extends ImagePreviewMatrixForSpring, ImagePreviewMatrixForExtra {}
+
 export interface ImagePreviewMatrixes {
-  matrixes: ImagePreviewMatrix[];
+  matrixes: { [imageId: number]: ImagePreviewMatrix | undefined };
 }
 
 const initialState: ImagePreviewMatrixes = {
-  matrixes: [],
+  matrixes: {},
+};
+
+const generateInitialImagePreviewMatrix = (merge?: Partial<ImagePreviewMatrix>): ImagePreviewMatrix => {
+  return {
+    imageId: 0,
+    x: undefined,
+    y: undefined,
+    scale: undefined,
+    reverseHorizontal: false,
+    reverseVertical: false,
+    rotate: 0,
+    ...merge,
+  };
 };
 
 export const ImagePreviewMatrixSlice = createSlice({
@@ -23,16 +42,76 @@ export const ImagePreviewMatrixSlice = createSlice({
   initialState,
   reducers: {
     updateMatrix: (state, action: PayloadAction<ImagePreviewMatrix>) => {
-      const exists = state.matrixes.find((m) => m.imageId === action.payload.imageId);
+      const exists = state.matrixes[action.payload.imageId];
       if (exists) {
-        exists.scale = action.payload.scale;
-        exists.x = action.payload.x;
-        exists.y = action.payload.y;
+        const obj = action.payload;
+        exists.reverseHorizontal = obj.reverseHorizontal;
+        exists.reverseVertical = obj.reverseVertical;
+        exists.rotate = obj.rotate;
+        exists.scale = obj.scale;
+        exists.x = obj.x;
+        exists.y = obj.y;
       } else {
-        state.matrixes.push(action.payload);
+        state.matrixes[action.payload.imageId] = action.payload;
+      }
+    },
+    updateMatrixForSpring: (state, action: PayloadAction<ImagePreviewMatrixForSpring>) => {
+      const exists = state.matrixes[action.payload.imageId];
+      if (exists) {
+        const obj = action.payload;
+        exists.scale = obj.scale;
+        exists.x = obj.x;
+        exists.y = obj.y;
+      } else {
+        state.matrixes[action.payload.imageId] = {
+          ...generateInitialImagePreviewMatrix(),
+          ...action.payload,
+        };
+      }
+    },
+    updateMatrixForExtra: (state, action: PayloadAction<ImagePreviewMatrixForExtra>) => {
+      const exists = state.matrixes[action.payload.imageId];
+      if (exists) {
+        const obj = action.payload;
+        exists.reverseHorizontal = obj.reverseHorizontal;
+        exists.reverseVertical = obj.reverseVertical;
+        exists.rotate = obj.rotate;
+      } else {
+        state.matrixes[action.payload.imageId] = {
+          ...generateInitialImagePreviewMatrix(),
+          ...action.payload,
+        };
       }
     },
   },
+  selectors: {
+    getSpringMatrix: createSelector(
+      [
+        (_, imageId: number) => imageId,
+        (state: ImagePreviewMatrixes, imageId: number) => state.matrixes[imageId]?.x,
+        (state: ImagePreviewMatrixes, imageId: number) => state.matrixes[imageId]?.y,
+        (state: ImagePreviewMatrixes, imageId: number) => state.matrixes[imageId]?.scale,
+      ],
+      (imageId, x, y, scale): ImagePreviewMatrixForSpring => {
+        return { imageId, x, y, scale };
+      },
+    ),
+    getExtraMatrix: createSelector(
+      [
+        (_, imageId: number) => imageId,
+        (state: ImagePreviewMatrixes, imageId: number) => state.matrixes[imageId]?.rotate,
+        (state: ImagePreviewMatrixes, imageId: number) => state.matrixes[imageId]?.reverseHorizontal,
+        (state: ImagePreviewMatrixes, imageId: number) => state.matrixes[imageId]?.reverseVertical,
+      ],
+      (imageId, rotate, reverseHorizontal, reverseVertical): ImagePreviewMatrixForExtra => {
+        if (rotate === undefined || reverseHorizontal === undefined || reverseVertical === undefined) {
+          return generateInitialImagePreviewMatrix({ imageId });
+        }
+        return { imageId, rotate, reverseHorizontal, reverseVertical };
+      },
+    ),
+  },
 });
 export default ImagePreviewMatrixSlice.reducer;
-export const { updateMatrix } = ImagePreviewMatrixSlice.actions;
+export const { updateMatrix, updateMatrixForExtra, updateMatrixForSpring } = ImagePreviewMatrixSlice.actions;
+export const { getSpringMatrix, getExtraMatrix } = ImagePreviewMatrixSlice.selectors;
